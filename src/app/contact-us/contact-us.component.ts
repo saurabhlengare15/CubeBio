174ba7c1b14eeed3c503589242ba121d74b44dfe
saurabhlengare15 <saurabhlengare15@gmail.com>
2023-12-17 20:15:05 +0530

    contact us solved

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -12,16 +12,8 @@ export class ContactUsComponent implements OnInit {
   constructor() { }
 
   ngOnInit(): void {
-<<<<<<< HEAD
-    // Aos.init({ once: true });
-    // window.addEventListener('load', Aos.refresh);
     Aos.init();
-    window.addEventListener('load',Aos.refresh);
-=======
-    Aos.init({ once: true });
-    window.addEventListener('load', Aos.refresh);
->>>>>>> 0ea201ac98d2273ffec3840f8abf17a5ff8ca801
-  }
+    window.addEventListener('load',Aos.refresh);}
 
   icons = ['fa fa-phone-square fa-3x','fa fa-envelope fa-3x','fa fa-map fa-3x', 'fa fa-map-marker fa-3x'];
   titles = ['Phone Number','Email','REGISTERED OFFICE ADDRESS','CORPORATE OFFICE'];
