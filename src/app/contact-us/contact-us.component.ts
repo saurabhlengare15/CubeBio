0d0ef498a421d0caa413b3a03c74e349d0f13477
saurabhlengare15 <saurabhlengare15@gmail.com>
2023-12-18 00:02:32 +0530

    contact us updated

--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -15,10 +15,10 @@ export class ContactUsComponent implements OnInit {
     Aos.init();
     window.addEventListener('load',Aos.refresh);}
 
-  icons = ['fa fa-phone-square fa-3x','fa fa-envelope fa-3x','fa fa-map fa-3x', 'fa fa-map-marker fa-3x'];
+  icons = ['fa fa-phone-square fa-4x','fa fa-envelope fa-4x','fa fa-map fa-4x', 'fa fa-thumb-tack fa-4x'];
   titles = ['Phone Number','Email','REGISTERED OFFICE ADDRESS','CORPORATE OFFICE'];
   sub_titles = ['040-40057965','INFO@CUBEBIOENERGY.CO.IN',
-              'D.NO. 17-105A, ALLAMVARIDONKA,KOLLIPARA VILLAGE AND MANDAL, GUNTUR DISTRICT, ANDHRA PRADESH, INDIA – 522304.',
+              'D.NO. 17-105A, ALLAMVARIDONKAw,KOLLIPARA VILLAGE AND MANDAL, GUNTUR DISTRICT, ANDHRA PRADESH, INDIA – 522304.',
               'CUBE BIO-ENERGY PRIVATE LIMITED, #501, KK PLAZA, AYYAPA SOCIETY, MADHAPUR, HYDERABAD – 500081.'
             ];
 
