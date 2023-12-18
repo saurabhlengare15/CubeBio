861ce9eb2c79a2e74fb850ea7cf5befc723385c9
saurabhlengare15 <saurabhlengare15@gmail.com>
2023-12-18 19:36:19 +0530

    about us updated

--- src/app/history/history.component.ts
+++ src/app/history/history.component.ts
@@ -1,10 +1,30 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+// @ts-ignore
+import Typewriter from 't-writer.js';
+// import Typewriter from 'typewriter-effect';
 
 @Component({
   selector: 'app-history',
   templateUrl: './history.component.html',
   styleUrls: ['./history.component.css']
 })
-export class HistoryComponent {
+export class HistoryComponent implements OnInit {
+  ngOnInit(): void {
+    const target = document.querySelector('.tw');
+
+    const writer = new Typewriter(target, {
+      loop: true,
+      typeColor: '#fff',
+      animateCursor:true,
+      blinkSpeed:500,
+      cursorColor: '#fff',
+    })
+    
+    writer
+      .type('about us')
+      .rest(500)
+      .start()
+
+  }
 
 }
