4ed61cb0deca65aacfcd4704ad0eacd785b0390c
saurabhlengare15 <saurabhlengare15@gmail.com>
2023-12-18 17:31:12 +0530

    iec updated

--- src/app/iec/iec.component.ts
+++ src/app/iec/iec.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { OwlOptions } from 'ngx-owl-carousel-o';
 
 @Component({
   selector: 'app-iec',
@@ -6,5 +7,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./iec.component.css']
 })
 export class IecComponent {
-
+  customOptions: OwlOptions = {
+    loop: true,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: false,
+    autoplay: true,
+    autoplaySpeed: 3000,
+    // autoplayTimeout: 2000,
+    navSpeed: 700,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 2
+      },
+      400: {
+        items: 3
+      },
+      740: {
+        items: 3
+      },
+      940: {
+        items: 4
+      }
+    },
+    nav: true
+  }
 }
