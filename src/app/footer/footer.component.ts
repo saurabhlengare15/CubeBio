336b64ccb23946d78c66cc4447c3356a63aab698
saurabhlengare15 <saurabhlengare15@gmail.com>
2023-12-30 23:52:38 +0530

    fised some minor mistakes

--- src/app/footer/footer.component.ts
+++ src/app/footer/footer.component.ts
@@ -1,4 +1,5 @@
-import { Component } from '@angular/core';
+import { Component,ElementRef, HostListener, Renderer2 } from '@angular/core';
+import { Router } from '@angular/router';
 
 @Component({
   selector: 'app-footer',
@@ -7,4 +8,10 @@ import { Component } from '@angular/core';
 })
 export class FooterComponent {
 
+  constructor(private router: Router, private renderer: Renderer2, private el: ElementRef) { }
+
+  showSoln(event:any){
+    const solution = event.target.value;
+     this.router.navigate(['/solutions',solution])
+  }
 }
