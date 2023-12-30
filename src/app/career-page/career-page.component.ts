1065f97c2b4fc65b2e07b9adb50ec80be7471c2a
NISHANT ULHARE <57897383+Nishant0210@users.noreply.github.com>
2023-12-30 14:28:16 +0530

    email conf. done

--- src/app/career-page/career-page.component.ts
+++ src/app/career-page/career-page.component.ts
@@ -1,8 +1,10 @@
 import { animate, state, style, transition, trigger } from '@angular/animations';
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import * as Aos from 'aos';
 // @ts-ignore
 import Typewriter from 't-writer.js';
+import emailjs from '@emailjs/browser';
 
 @Component({
   selector: 'app-career-page',
@@ -48,9 +50,74 @@ export class CareerPageComponent implements OnInit{
     window.addEventListener('load', Aos.refresh);
   }
 
+  constructor(private fb : FormBuilder){}
+
+  success:boolean = false;
+  error:boolean = false;
+
   buttonState = 'notHovered';
   OnHover(hovered: boolean) {
     this.buttonState = hovered ? 'hovered' : 'notHovered';
+  }
+
+
+  myForm: FormGroup = this.fb.group({
+    // form controls and validation rules here
+    name: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]],
+    phone: ['', [Validators.required, Validators.pattern(/^[1-9]\d{9}$/)]],
+    address: ['', [Validators.required]],
+  });
+
+  OnSubmit(){
+
+    if (this.myForm.valid) {
+      console.log(this.myForm.value);
+
+      const templateParams = {
+        name: this.myForm.value.name,
+        email: this.myForm.value.email,
+        phone: this.myForm.value.phone,
+        address: this.myForm.value.address,
+    };
+
+    emailjs.send('service_pk991yc','template_yc7im6c', templateParams,'kg9RpK0xdqiOA6W3h')
+      .then((response) => {
+         console.log('SUCCESS!', response.status, response.text);
+         this.success = true;
+        this.error = false;
+      }, (err) => {
+         console.log('FAILED...', err);
+      });
+
+      this.myForm.reset();
+
+    } else {
+      console.log('Form is invalid. Please fix the errors.');
+      console.log(this.myForm.value);
+      this.error = true;
+      this.success = false;
+    }
+
+  }
+
+  closeAlert(){
+    this.success = false;
+    this.error = false;
+  }
+
+  hasError(controlName: string, errorName: string) {
+    const control = this.myForm.get(controlName);
+
+    if (!control) {
+      return false;
+    }
+
+    if (errorName === 'pattern' && control.hasError(errorName)) {
+      return control.dirty || control.touched;
+    }
+
+    return control.hasError(errorName) && (control.dirty || control.touched);
   }
 
 }
--- src/app/contact-us/contact-us.component.ts
+++ src/app/contact-us/contact-us.component.ts
@@ -1,8 +1,11 @@
 import { Component, OnInit } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 import { NavigationEnd, Router } from '@angular/router';
 import * as Aos from 'aos';
 // @ts-ignore
 import Typewriter from 't-writer.js';
+import emailjs from '@emailjs/browser';
+
 
 @Component({
   selector: 'app-contact-us',
@@ -11,7 +14,10 @@ import Typewriter from 't-writer.js';
 })
 export class ContactUsComponent implements OnInit {
 
-  constructor(private router: Router) {
+  success:boolean = false;
+  error:boolean = false;
+
+  constructor(private router: Router, private fb : FormBuilder) {
     this.router.routeReuseStrategy.shouldReuseRoute = function () {
       return false;
     }
@@ -46,6 +52,63 @@ export class ContactUsComponent implements OnInit {
     window.addEventListener('load', Aos.refresh);
 
   }
+
+  myForm: FormGroup = this.fb.group({
+    // form controls and validation rules here
+    name: ['', [Validators.required]],
+    email: ['', [Validators.required, Validators.email]],
+    phone: ['', [Validators.required, Validators.pattern(/^[1-9]\d{9}$/)]],
+    subject: ['', [Validators.required]],
+    message: ['', [Validators.required]],
+  });
+
+  OnSubmit(){
+
+    if (this.myForm.valid) {
+      console.log(this.myForm.value);
+
+      const templateParams = {
+        name: this.myForm.value.name,
+        email: this.myForm.value.email,
+        phone: this.myForm.value.phone,
+        subject: this.myForm.value.subject,
+        message: this.myForm.value.message
+    };
+
+    emailjs.send('service_pk991yc','template_tlsflo9', templateParams,'kg9RpK0xdqiOA6W3h')
+      .then((response) => {
+         console.log('SUCCESS!', response.status, response.text);
+         this.success = true;
+        this.error = false;
+      }, (err) => {
+         console.log('FAILED...', err);
+      });
+
+      this.myForm.reset();
+
+    } else {
+      console.log('Form is invalid. Please fix the errors.');
+      console.log(this.myForm.value);
+      this.error = true;
+      this.success = false;
+    }
+
+  }
+
+  hasError(controlName: string, errorName: string) {
+    const control = this.myForm.get(controlName);
+
+    if (!control) {
+      return false;
+    }
+
+    if (errorName === 'pattern' && control.hasError(errorName)) {
+      return control.dirty || control.touched;
+    }
+
+    return control.hasError(errorName) && (control.dirty || control.touched);
+  }
+
 // CUBE BIO-ENERGY PRIVATE LIMITED, #501, KK PLAZA, AYYAPA SOCIETY, MADHAPUR, HYDERABAD – 500081.
   icons = ['fa fa-phone-square fa-4x', 'fa fa-envelope fa-4x', 'fa fa-map fa-4x', 'fa fa-thumb-tack fa-4x'];
   titles = ['Phone Number', 'Email', 'REGISTERED OFFICE ADDRESS', 'CORPORATE OFFICEs'];
@@ -53,5 +116,10 @@ export class ContactUsComponent implements OnInit {
     'ECP VASTU A2 | 16, HANDEWADI ROAD, HADAPSAR PUNE - 411028',
     'TELANGANA - KHAMMAM, NIZAMABAD, KOTHAGUDAM, CHITTAPUR, MAHARASHTRA - RAHU, PUNE'
   ];
+
+  closeAlert(){
+    this.success = false;
+    this.error = false;
+  }
 
 }
